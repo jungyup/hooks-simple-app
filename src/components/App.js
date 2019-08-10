50ed8256402e9e1de94890a70184b68fc4788f51
jungyup <jungyupkim4@gmail.com>
2019-08-09 20:07:53 -0400

    first commit, set up the environment and add components

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const App = () => {
+
+    // array destructureing
+    const [resource, setResource] = useState('posts');
+    
+    return (
+        <div>
+            <div>
+                <button className="ui button primary" onClick={() => setResource('posts')}>Posts</button>
+                <button className="ui button secondary" onClick={() => setResource('todos')}>Todos</button>
+            </div>
+            {resource}
+        </div>
+    );
+}
+
+export default App;
